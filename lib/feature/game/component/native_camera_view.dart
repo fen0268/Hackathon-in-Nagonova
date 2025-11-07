0f3e9aa0f5ec1901cfa205acc3b59d254cab3ba9
fen0268 <Kenshi150107@gmail.com>
2025-11-08 06:19:29 +0900

    feat: ネイティブのカメラ起動中のプレビュー画面の上にカウントダウンUI表示実装

---
+++ lib/feature/game/component/native_camera_view.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// ネイティブカメラビューを表示するウィジェット
+/// iOS: UiKitView、Android: AndroidView を使用
+class NativeCameraView extends StatelessWidget {
+  const NativeCameraView({super.key});
+
+  static const String viewType = 'com.example.hackathon_app/camera_view';
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return const UiKitView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: <String, dynamic>{},
+        creationParamsCodec: StandardMessageCodec(),
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(
+            EagerGestureRecognizer.new,
+          ),
+        },
+      );
+    } else if (Platform.isAndroid) {
+      return const AndroidView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: <String, dynamic>{},
+        creationParamsCodec: StandardMessageCodec(),
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(
+            EagerGestureRecognizer.new,
+          ),
+        },
+      );
+    } else {
+      return const Center(
+        child: Text(
+          'このプラットフォームはサポートされていません',
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    }
+  }
+}
