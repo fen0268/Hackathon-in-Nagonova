da16d7f1b83f2589042370bc50c38c89c0103add
fen0268 <Kenshi150107@gmail.com>
2025-11-07 17:54:14 +0900

    feat: ホームページ基盤実装

---
+++ lib/feature/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
+  HomePageState createConsumerState() => HomePageState();
+
+  static const routeName = 'home';
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends ConsumerState<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
