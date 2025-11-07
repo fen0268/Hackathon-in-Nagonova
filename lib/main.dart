808c28b3d95e8a535244f4d1bcb7929b84818852
fen0268 <Kenshi150107@gmail.com>
2025-11-07 18:40:46 +0900

    feat: firebase 初期化処理実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hackathon_app/app.dart';
 
+import 'config/firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    ProviderScope(
+    const ProviderScope(
       child: App(),
     ),
   );
